9227432a6e8b565e9a2105d117ae6750c8f9f650
ngicks <yknt.bsl@gmail.com>
2024-09-06 19:17:41 +0000

    🐛: fix not being actually infinite iterator

--- hiter/repeat.go
+++ hiter/repeat.go
@@ -5,6 +5,15 @@ import "iter"
 // Repeat returns an iterator that generates v n times.
 // If n < 0, the returned iterator repeats forever.
 func Repeat[V any](v V, n int) iter.Seq[V] {
+	if n < 0 {
+		return func(yield func(V) bool) {
+			for {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
 	return func(yield func(V) bool) {
 		for ; n != 0; n-- {
 			if !yield(v) {
@@ -17,6 +26,15 @@ func Repeat[V any](v V, n int) iter.Seq[V] {
 // Repeat2 returns an iterator that generates v n times.
 // If n < 0, the returned iterator repeats forever.
 func Repeat2[K, V any](k K, v V, n int) iter.Seq2[K, V] {
+	if n < 0 {
+		return func(yield func(K, V) bool) {
+			for {
+				if !yield(k, v) {
+					return
+				}
+			}
+		}
+	}
 	return func(yield func(K, V) bool) {
 		for ; n != 0; n-- {
 			if !yield(k, v) {
@@ -29,6 +47,15 @@ func Repeat2[K, V any](k K, v V, n int) iter.Seq2[K, V] {
 // RepeatFunc returns an iterator that generates result from fnV n times.
 // If n < 0, the returned iterator repeats forever.
 func RepeatFunc[V any](fnV func() V, n int) iter.Seq[V] {
+	if n < 0 {
+		return func(yield func(V) bool) {
+			for {
+				if !yield(fnV()) {
+					return
+				}
+			}
+		}
+	}
 	return func(yield func(V) bool) {
 		for ; n != 0; n-- {
 			if !yield(fnV()) {
@@ -41,6 +68,15 @@ func RepeatFunc[V any](fnV func() V, n int) iter.Seq[V] {
 // RepeatFunc2 returns an iterator that generates result of fnK and fnV n times.
 // If n < 0, the returned iterator repeats forever.
 func RepeatFunc2[K, V any](fnK func() K, fnV func() V, n int) iter.Seq2[K, V] {
+	if n < 0 {
+		return func(yield func(K, V) bool) {
+			for {
+				if !yield(fnK(), fnV()) {
+					return
+				}
+			}
+		}
+	}
 	return func(yield func(K, V) bool) {
 		for ; n != 0; n-- {
 			if !yield(fnK(), fnV()) {

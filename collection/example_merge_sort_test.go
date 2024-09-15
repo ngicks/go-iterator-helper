fc187c390d5fb8cdaca90f6723a77f31538148b9
ngicks <yknt.bsl@gmail.com>
2024-09-15 05:57:26 +0000

    ✨: expose MergeSort under collection package

---
+++ collection/example_merge_sort_test.go
@@ -0,0 +1,123 @@
+package collection_test
+
+import (
+	"cmp"
+	"fmt"
+	"iter"
+	"math/rand/v2"
+	"slices"
+
+	"github.com/ngicks/go-iterator-helper/collection"
+	"github.com/ngicks/go-iterator-helper/hiter"
+	"github.com/ngicks/go-iterator-helper/x/exp/xiter"
+)
+
+// avoiding xter dep
+func limit[V any](seq iter.Seq[V], n int) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		if n <= 0 {
+			return
+		}
+		for v := range seq {
+			if !yield(v) {
+				return
+			}
+			if n--; n <= 0 {
+				break
+			}
+		}
+	}
+}
+
+func mergeSortFunc[S ~[]T, T any](m S, cmp func(l, r T) int) S {
+	if len(m) <= 1 {
+		return m
+	}
+	left, right := m[:len(m)/2], m[len(m)/2:]
+	left = mergeSortFunc(left, cmp)
+	right = mergeSortFunc(right, cmp)
+	return mergeFunc(left, right, cmp)
+}
+
+func mergeFunc[S ~[]T, T any](l, r S, cmp func(l, r T) int) S {
+	m := make(S, len(l)+len(r))
+	var i int
+	for i = 0; len(l) > 0 && len(r) > 0; i++ {
+		if cmp(l[0], r[0]) < 0 {
+			m[i] = l[0]
+			l = l[1:]
+		} else {
+			m[i] = r[0]
+			r = r[1:]
+		}
+	}
+	for _, t := range l {
+		m[i] = t
+		i++
+	}
+	for _, t := range r {
+		m[i] = t
+		i++
+	}
+	return m
+}
+
+var _ collection.SliceLike[any] = sliceAdapter[any]{}
+
+type sliceAdapter[T any] []T
+
+func (s sliceAdapter[T]) At(i int) T {
+	return s[i]
+}
+
+func (s sliceAdapter[T]) Len() int {
+	return len(s)
+}
+
+// Example_merge_sort implements slice version merge sort and re-implements iterator version of it.
+func Example_merge_sort() {
+	rng := hiter.RepeatFunc(func() int { return rand.N(20) }, -1)
+	fmt.Printf("merge sort: %t\n",
+		slices.IsSorted(mergeSortFunc(slices.Collect(limit(rng, 10)), cmp.Compare)),
+	)
+	fmt.Printf(
+		"merge sort iter: %t\n",
+		slices.IsSorted(
+			slices.Collect(
+				collection.MergeSort(slices.Collect(limit(rng, 10))),
+			),
+		),
+	)
+	fmt.Printf(
+		"merge sort atter: %t\n",
+		slices.IsSorted(
+			slices.Collect(
+				collection.MergeSortSliceLike(sliceAdapter[int](slices.Collect(limit(rng, 10)))),
+			),
+		),
+	)
+	fmt.Printf(
+		"merge sort atter logically concatenated: %t\n",
+		slices.IsSorted(
+			slices.Collect(
+				collection.MergeSortSliceLike(
+					collection.ConcatSliceLike(
+						slices.Collect(
+							xiter.Map(
+								func(i int) collection.SliceLike[int] {
+									return sliceAdapter[int](slices.Collect(limit(rng, i)))
+								},
+								xiter.Limit(rng, 5),
+							),
+						)...,
+					),
+				),
+			),
+		),
+	)
+	// Output:
+	// merge sort: true
+	// merge sort iter: true
+	// merge sort atter: true
+	// merge sort atter logically concatenated: true
+}
---
+++ collection/merge_sort.go
@@ -0,0 +1,157 @@
+package collection
+
+import (
+	"cmp"
+	"fmt"
+	"iter"
+	"slices"
+
+	"github.com/ngicks/go-iterator-helper/hiter"
+	"github.com/ngicks/go-iterator-helper/x/exp/xiter"
+)
+
+// MergeSort implements merge sort algorithm.
+func MergeSort[S ~[]T, T cmp.Ordered](m S) iter.Seq[T] {
+	return MergeSortFunc(m, cmp.Compare)
+}
+
+func MergeSortFunc[S ~[]T, T any](m S, cmp func(l, r T) int) iter.Seq[T] {
+	if len(m) <= 1 {
+		return slices.Values(m)
+	}
+	// TODO: We might want to implement non-recursive version? AFAIK Go does not optimize for recursion.
+	return func(yield func(T) bool) {
+		left, right := m[:len(m)/2], m[len(m)/2:]
+		leftIter := MergeSortFunc(left, cmp)
+		rightIter := MergeSortFunc(right, cmp)
+		for t := range xiter.MergeFunc(leftIter, rightIter, cmp) {
+			if !yield(t) {
+				return
+			}
+		}
+	}
+}
+
+func MergeSortSliceLike[S SliceLike[T], T cmp.Ordered](s S) iter.Seq[T] {
+	return MergeSortSliceLikeFunc(s, cmp.Compare)
+}
+
+func MergeSortSliceLikeFunc[S SliceLike[T], T any](s S, cmp func(l, r T) int) iter.Seq[T] {
+	return mergeSortSubbableFunc(newSubbable(s), cmp)
+}
+
+type SliceLike[T any] interface {
+	hiter.Atter[T]
+	Len() int
+}
+
+// ConcatSliceLike logically concatenates [SliceLike][T] implementations to form a single concatenated [SliceLike][T].
+func ConcatSliceLike[T any](s ...SliceLike[T]) SliceLike[T] {
+	var (
+		ss          []indexRanged[T]
+		totalLength int
+	)
+
+	for _, s := range s {
+		ss = append(ss, indexRanged[T]{
+			Start: totalLength,
+			End:   totalLength + s.Len(),
+			S:     s,
+		})
+		totalLength += s.Len()
+	}
+
+	return combiner[T]{ss, totalLength}
+}
+
+type combiner[T any] struct {
+	ss          []indexRanged[T]
+	totalLength int
+}
+
+type indexRanged[T any] struct {
+	Start, End int
+	S          SliceLike[T]
+}
+
+func compareRange[T any](r indexRanged[T], t int) int {
+	switch {
+	case t < r.Start:
+		return 1
+	case r.Start <= t && t < r.End:
+		return 0
+	default: // r.End <= off:
+		return -1
+	}
+}
+
+func (c combiner[T]) At(i int) T {
+	// I took benchmarks and noticed that if ss is holding less than like 20 elements,
+	// linear search is slightly faster (2.5 ns vs 8 ns).
+	// At least for Go 1.23.0.
+	// But I don't care about that here.
+	found, ok := slices.BinarySearchFunc(c.ss, i, compareRange)
+	if !ok {
+		panic(fmt.Sprintf("index out of range [%d], with length of %d", i, c.totalLength))
+	}
+	target := c.ss[found]
+	return target.S.At(i - target.Start)
+}
+
+func (c combiner[T]) Len() int {
+	return c.totalLength
+}
+
+func mergeSortSubbableFunc[S SliceLike[T], T any](s subbable[S, T], cmp func(l, r T) int) iter.Seq[T] {
+	if s.Len() <= 1 {
+		return hiter.OmitF(hiter.IndexAccessible(s, hiter.Range(0, s.Len())))
+	}
+	return func(yield func(T) bool) {
+		left, right := s.Sub(0, s.Len()/2), s.Sub(s.Len()/2, s.Len())
+		leftIter := mergeSortSubbableFunc(left, cmp)
+		rightIter := mergeSortSubbableFunc(right, cmp)
+		for t := range xiter.MergeFunc(leftIter, rightIter, cmp) {
+			if !yield(t) {
+				return
+			}
+		}
+	}
+}
+
+type subbable[S SliceLike[T], T any] struct {
+	S    S
+	i, j int
+}
+
+func newSubbable[S SliceLike[T], T any](s S) subbable[S, T] {
+	return subbable[S, T]{
+		S: s,
+		i: 0,
+		j: s.Len(),
+	}
+}
+
+func (s subbable[S, T]) At(i int) T {
+	i = s.i + i
+	if i < s.i || i >= s.j {
+		panic(fmt.Sprintf("index out of range [%d], with range of [%d, %d)", i, s.i, s.j))
+	}
+	return s.S.At(i)
+}
+
+func (s subbable[S, T]) Len() int {
+	return s.j - s.i
+}
+
+func (s subbable[S, T]) Sub(i, j int) subbable[S, T] {
+	i = i + s.i
+	j = j + s.i
+	if i < s.i || j > s.j || i > j {
+		panic(fmt.Errorf("slice bounds out of range [%d:%d] with range [%d, %d)", i, j, s.i, s.j))
+	}
+	return subbable[S, T]{
+		S: s.S,
+		i: i,
+		j: j,
+	}
+}

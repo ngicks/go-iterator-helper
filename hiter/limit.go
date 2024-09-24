d46b974645e4fac55210d3b8cdda1e2adf06d12c
ngicks <yknt.bsl@gmail.com>
2024-09-24 00:14:46 +0000

    ✨: add LimitAfter
    They are useful for iter.Seq[V, error]

--- hiter/limit.go
+++ hiter/limit.go
@@ -23,3 +23,33 @@ func LimitUntil2[K, V any](f func(K, V) bool, seq iter.Seq2[K, V]) iter.Seq2[K, 
 		}
 	}
 }
+
+// LimitAfter is like [LimitUntil] but also yields the first value dissatisfying f(v).
+func LimitAfter[V any](f func(V) bool, seq iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for v := range seq {
+			if !f(v) {
+				yield(v)
+				return
+			}
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+
+// LimitAfter2 is like [LimitUntil2] but also yields the first pair dissatisfying f(k, v).
+func LimitAfter2[K, V any](f func(K, V) bool, seq iter.Seq2[K, V]) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range seq {
+			if !f(k, v) {
+				yield(k, v)
+				return
+			}
+			if !yield(k, v) {
+				return
+			}
+		}
+	}
+}
--- hiter/limit_test.go
+++ hiter/limit_test.go
@@ -8,34 +8,71 @@ import (
 	"github.com/ngicks/go-iterator-helper/hiter"
 )
 
-func TestLimitUntil(t *testing.T) {
-	src := []int{1, 1, 4, 2, 1, 1, 4, 2, 1, 1, 6, 2, 1, 1, 9, 2}
-	t.Run("LimitUntil", func(t *testing.T) {
-		testCase1[int]{
-			Seq: func() iter.Seq[int] {
-				return hiter.LimitUntil(func(e int) bool { return e < 6 }, slices.Values(src))
-			},
-			Expected: []int{1, 1, 4, 2, 1, 1, 4, 2, 1, 1},
-			BreakAt:  3,
-		}.Test(t)
-	})
+var (
+	limitUntilSrc = []int{1, 1, 4, 2, 1, 1, 4, 2, 1, 1, 6, 2, 1, 1, 9, 2}
+)
 
-	t.Run("LimitUntil2", func(t *testing.T) {
+func TestLimitUntil(t *testing.T) {
+	testCase1[int]{
+		Seq: func() iter.Seq[int] {
+			return hiter.LimitUntil(func(e int) bool { return e < 6 }, slices.Values(limitUntilSrc))
+		},
+		Expected: []int{1, 1, 4, 2, 1, 1, 4, 2, 1, 1},
+		BreakAt:  3,
+	}.Test(t)
+}
+
+func TestLimitAfter(t *testing.T) {
+	testCase1[int]{
+		Seq: func() iter.Seq[int] {
+			return hiter.LimitAfter(func(e int) bool { return e < 6 }, slices.Values(limitUntilSrc))
+		},
+		Expected: []int{1, 1, 4, 2, 1, 1, 4, 2, 1, 1, 6},
+		BreakAt:  3,
+	}.Test(t)
+}
+
+func TestLimitUntil2(t *testing.T) {
+
+	t.Run("limit by value", func(t *testing.T) {
 		testCase2[int, int]{
 			Seq: func() iter.Seq2[int, int] {
-				return hiter.LimitUntil2(func(i, e int) bool { return e < 6 }, slices.All(src))
+				return hiter.LimitUntil2(func(i, e int) bool { return e < 6 }, slices.All(limitUntilSrc))
 			},
 			Expected: []hiter.KeyValue[int, int]{{0, 1}, {1, 1}, {2, 4}, {3, 2}, {4, 1}, {5, 1}, {6, 4}, {7, 2}, {8, 1}, {9, 1}},
 			BreakAt:  3,
 		}.Test(t)
 	})
 
-	t.Run("LimitUntil2", func(t *testing.T) {
+	t.Run("limit by key", func(t *testing.T) {
 		testCase2[int, int]{
 			Seq: func() iter.Seq2[int, int] {
-				return hiter.LimitUntil2(func(i, e int) bool { return i < 3 }, slices.All(src))
+				return hiter.LimitUntil2(func(i, e int) bool { return i < 3 }, slices.All(limitUntilSrc))
 			},
 			Expected: []hiter.KeyValue[int, int]{{0, 1}, {1, 1}, {2, 4}},
+			BreakAt:  2,
+		}.Test(t)
+	})
+}
+
+func TestLimitAfter2(t *testing.T) {
+
+	t.Run("limit by value", func(t *testing.T) {
+		testCase2[int, int]{
+			Seq: func() iter.Seq2[int, int] {
+				return hiter.LimitAfter2(func(i, e int) bool { return e < 4 }, slices.All(limitUntilSrc))
+			},
+			Expected: []hiter.KeyValue[int, int]{{0, 1}, {1, 1}, {2, 4}},
+			BreakAt:  3,
+		}.Test(t)
+	})
+
+	t.Run("limit by key", func(t *testing.T) {
+		testCase2[int, int]{
+			Seq: func() iter.Seq2[int, int] {
+				return hiter.LimitAfter2(func(i, e int) bool { return i < 3 }, slices.All(limitUntilSrc))
+			},
+			Expected: []hiter.KeyValue[int, int]{{0, 1}, {1, 1}, {2, 4}, {3, 2}},
 			BreakAt:  2,
 		}.Test(t)
 	})

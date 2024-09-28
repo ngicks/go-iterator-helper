547df8b765ad4189794e9301bd76acaf8dacc6d2
ngicks <yknt.bsl@gmail.com>
2024-09-28 08:43:09 +0000

    📝: add doc comment for Decode

--- hiter/io_read.go
+++ hiter/io_read.go
@@ -5,6 +5,10 @@ import (
 	"iter"
 )
 
+// Decode returns an iterator over consecutive decode results of dec.
+//
+// The iterator stops if and only if dec returns io.EOF. Handling other errors is caller's responsibility.
+// If the first error should stop the iterator, use [LimitUntil], [LimitAfter] or [*errbox.Box].
 func Decode[V any, Dec interface{ Decode(any) error }](dec Dec) iter.Seq2[V, error] {
 	return func(yield func(V, error) bool) {
 		for {

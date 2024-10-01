08ffe01f121bd78e327d421f6be823090b43899a
ngicks <yknt.bsl@gmail.com>
2024-10-01 09:57:16 +0000

    ✨: add FlattenSeq variants

--- hiter/flatten.go
+++ hiter/flatten.go
@@ -2,7 +2,7 @@ package hiter
 
 import "iter"
 
-// Flatten returns an iterator over slices yielded from seq.
+// Flatten returns an iterator over each value of slices from seq.
 func Flatten[S ~[]E, E any](seq iter.Seq[S]) iter.Seq[E] {
 	return func(yield func(E) bool) {
 		for s := range seq {
@@ -15,8 +15,34 @@ func Flatten[S ~[]E, E any](seq iter.Seq[S]) iter.Seq[E] {
 	}
 }
 
-// FlattenF returns an iterator over pairs of slice and non-slice.
-// While iterating over slices, the latter part of pair is repeated until the slice is fully consumed.
+// Flatten returns and iterator over values from iterators from seq.
+func FlattenSeq[V any](seq iter.Seq[iter.Seq[V]]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for s := range seq {
+			for t := range s {
+				if !yield(t) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// FlattenSeq2 returns and iterator over key-value pairs from iterators from seq.
+func FlattenSeq2[K, V any](seq iter.Seq[iter.Seq2[K, V]]) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for s := range seq {
+			for k, v := range s {
+				if !yield(k, v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// FlattenF returns an iterator over flattened key-value pairs from seq.
+// While the iterator iterates over the former value from seq, the latter value is repeated.
 func FlattenF[S1 ~[]E1, E1 any, E2 any](seq iter.Seq2[S1, E2]) iter.Seq2[E1, E2] {
 	return func(yield func(E1, E2) bool) {
 		for s, g := range seq {
@@ -29,13 +55,41 @@ func FlattenF[S1 ~[]E1, E1 any, E2 any](seq iter.Seq2[S1, E2]) iter.Seq2[E1, E2]
 	}
 }
 
-// FlattenL returns an iterator over pairs of non-slice and slice.
-// While iterating over slices, the former part of pair is repeated until the slice is fully consumed.
+// FlattenL returns an iterator over flattened key-value pairs from seq.
+// While the iterator iterates over the latter value from seq, the former value is repeated.
 func FlattenL[S2 ~[]E2, E1 any, E2 any](seq iter.Seq2[E1, S2]) iter.Seq2[E1, E2] {
 	return func(yield func(E1, E2) bool) {
 		for e, t := range seq {
 			for _, g := range t {
 				if !yield(e, g) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// FlattenSeqF returns an iterator over flattened key-value pairs from seq.
+// While the iterator iterates over the former value from seq, the latter value is repeated.
+func FlattenSeqF[K, V any](seq iter.Seq2[iter.Seq[K], V]) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range seq {
+			for k := range k {
+				if !yield(k, v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// FlattenSeqF returns an iterator over flattened pairs from seq.
+// While the iterator iterates over the latter value from seq, the former value is repeated.
+func FlattenSeqL[K, V any](seq iter.Seq2[K, iter.Seq[V]]) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range seq {
+			for v := range v {
+				if !yield(k, v) {
 					return
 				}
 			}
--- hiter/flatten_test.go
+++ hiter/flatten_test.go
@@ -6,39 +6,100 @@ import (
 	"testing"
 
 	"github.com/ngicks/go-iterator-helper/hiter"
+	"github.com/ngicks/go-iterator-helper/x/exp/xiter"
 )
 
 func TestFlatten(t *testing.T) {
+	var (
+		nested1 = slices.Values([][]int{{0, 1, 2}, {3, 4}, {5}})
+		nested2 = slices.Values([][]int{{10}, {7, 8}, {1}})
+		flat1   = slices.Values([]int{10, 7, 8, 1})
+		flat2   = slices.Values([]int{0, 1, 2, 3, 4, 5})
+	)
+
+	flattenResult := []int{0, 1, 2, 3, 4, 5}
 	t.Run("Flatten", func(t *testing.T) {
 		testCase1[int]{
 			Seq: func() iter.Seq[int] {
-				return hiter.Flatten(slices.Values([][]int{{0, 1, 2}, {3, 4}, {5}}))
+				return hiter.Flatten(nested1)
 			},
-			Expected: []int{0, 1, 2, 3, 4, 5},
+			Expected: flattenResult,
 			BreakAt:  4,
 		}.Test(t)
 	})
+	t.Run("Flatten", func(t *testing.T) {
+		testCase1[int]{
+			Seq: func() iter.Seq[int] {
+				return hiter.FlattenSeq(xiter.Map(slices.Values, nested1))
+			},
+			Expected: flattenResult,
+			BreakAt:  4,
+		}.Test(t)
+	})
+
+	t.Run("FlattenSeq2", func(t *testing.T) {
+		testCase2[int, int]{
+			Seq: func() iter.Seq2[int, int] {
+				return hiter.FlattenSeq2(slices.Values(
+					[]iter.Seq2[int, int]{
+						hiter.Values2([]hiter.KeyValue[int, int]{{0, 1}, {2, 2}}),
+						hiter.Values2([]hiter.KeyValue[int, int]{{1, 0}, {4, 5}, {2, 9}}),
+					},
+				))
+			},
+			Expected: []hiter.KeyValue[int, int]{{0, 1}, {2, 2}, {1, 0}, {4, 5}, {2, 9}},
+			BreakAt:  2,
+		}.Test(t)
+	})
+
+	flattenFResult := []hiter.KeyValue[int, int]{{0, 10}, {1, 10}, {2, 10}, {3, 7}, {4, 7}, {5, 8}}
 	t.Run("FlattenF", func(t *testing.T) {
 		testCase2[int, int]{
 			Seq: func() iter.Seq2[int, int] {
 				return hiter.FlattenF(hiter.Pairs(
-					slices.Values([][]int{{0, 1, 2}, {3, 4}, {5}}),
-					slices.Values([]int{10, 7, 8, 1}),
+					nested1,
+					flat1,
 				))
 			},
-			Expected: []hiter.KeyValue[int, int]{{0, 10}, {1, 10}, {2, 10}, {3, 7}, {4, 7}, {5, 8}},
+			Expected: flattenFResult,
 			BreakAt:  4,
 		}.Test(t)
 	})
+	t.Run("FlattenSeqF", func(t *testing.T) {
+		testCase2[int, int]{
+			Seq: func() iter.Seq2[int, int] {
+				return hiter.FlattenSeqF(hiter.Pairs(
+					xiter.Map(slices.Values, nested1),
+					flat1,
+				))
+			},
+			Expected: flattenFResult,
+			BreakAt:  4,
+		}.Test(t)
+	})
+
+	flattenLResult := []hiter.KeyValue[int, int]{{0, 10}, {1, 7}, {1, 8}, {2, 1}}
 	t.Run("FlattenL", func(t *testing.T) {
 		testCase2[int, int]{
 			Seq: func() iter.Seq2[int, int] {
 				return hiter.FlattenL(hiter.Pairs(
-					slices.Values([]int{0, 1, 2, 3, 4, 5}),
-					slices.Values([][]int{{10}, {7, 8}, {1}}),
+					flat2,
+					nested2,
 				))
 			},
-			Expected: []hiter.KeyValue[int, int]{{0, 10}, {1, 7}, {1, 8}, {2, 1}},
+			Expected: flattenLResult,
+			BreakAt:  2,
+		}.Test(t)
+	})
+	t.Run("FlattenSeqL", func(t *testing.T) {
+		testCase2[int, int]{
+			Seq: func() iter.Seq2[int, int] {
+				return hiter.FlattenSeqL(hiter.Pairs(
+					flat2,
+					xiter.Map(slices.Values, nested2),
+				))
+			},
+			Expected: flattenLResult,
 			BreakAt:  2,
 		}.Test(t)
 	})

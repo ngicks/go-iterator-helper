d65d101c75af3320ce29a12f5cad107a6eefdb40
ngicks <yknt.bsl@gmail.com>
2024-11-02 10:41:52 +0000

    ⚡️: allow single Read call to read more than 2 elements

--- hiter/iterable/reader.go
+++ hiter/iterable/reader.go
@@ -3,13 +3,12 @@ package iterable
 import (
 	"io"
 	"iter"
-
-	"github.com/ngicks/go-iterator-helper/hiter"
 )
 
 // Reader returns a reader which reads values from seq marshaled by marshaler.
-// seq must be one-shot; each time Read is called one or two values are yielded from seq.
+// seq must be stateful and one-shot; each time Read is called one or more values are yielded from seq.
 // If seq is pure and reuseable, Read reads same value repeatedly.
+// You can wrap pure seq to be stateful by wrapping it with [NewResumable] or [NewPeekable].
 func Reader[V any](marshaler func(V) ([]byte, error), seq iter.Seq[V]) io.Reader {
 	return &iterReader[V]{
 		marshaler: marshaler,
@@ -29,26 +28,29 @@ func (r *iterReader[V]) Read(p []byte) (n int, err error) {
 		return 0, r.err
 	}
 	if len(r.buf) > 0 {
-		n = copy(p, r.buf)
-		p = p[n:]
-		r.buf = r.buf[n:]
-		if len(r.buf) > 0 {
+		nn := copy(p, r.buf)
+		n += nn
+		p = p[nn:]
+		r.buf = r.buf[nn:]
+		if len(p) == 0 {
 			return
 		}
 	}
-	next, ok := hiter.First(r.seq)
-	if !ok {
-		err = io.EOF
-		r.err = err
-		return
+	for v := range r.seq {
+		r.buf, err = r.marshaler(v)
+		if err != nil {
+			r.err = err
+			return
+		}
+		nn := copy(p, r.buf)
+		n += nn
+		p = p[nn:]
+		r.buf = r.buf[nn:]
+		if len(p) == 0 {
+			return
+		}
 	}
-	r.buf, err = r.marshaler(next)
-	if err != nil {
-		r.err = err
-		return
-	}
-	nn := copy(p, r.buf)
-	n += nn
-	r.buf = r.buf[nn:]
+	err = io.EOF
+	r.err = err
 	return
 }

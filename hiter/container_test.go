01df061a0432dca07df746c96643759d1fac8e2c
ngicks <yknt.bsl@gmail.com>
2024-11-28 11:24:01 +0000

    ✨: add Append{Heap,List,Ring}, Collect{List,Ring}

--- hiter/container_test.go
+++ hiter/container_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ngicks/go-iterator-helper/hiter"
 	"github.com/ngicks/go-iterator-helper/hiter/iterable"
+	"gotest.tools/v3/assert"
 )
 
 func TestContainerHeap(t *testing.T) {
@@ -140,4 +141,36 @@ func TestContainerRing(t *testing.T) {
 			BreakAt:  3,
 		}.Test(t)
 	})
+}
+
+func TestContainerCollect(t *testing.T) {
+	seq := hiter.Range(0, 10)
+
+	h1 := &sliceHeap{}
+	h2 := hiter.AppendHeapSeq(h1, seq)
+	assert.Assert(t, h1 == h2)
+	assert.DeepEqual(t, slices.Collect(seq), slices.Collect(hiter.Heap[int](h2)))
+
+	l1 := list.New()
+	for i := range hiter.Range(0, 3) {
+		l1.PushBack(i)
+	}
+	l2 := hiter.AppendListSeq(l1, hiter.Range(3, 10))
+	assert.Assert(t, l1 == l2)
+	assert.DeepEqual(t, slices.Collect(seq), slices.Collect(hiter.ListAll[int](l2)))
+	l3 := hiter.CollectList(hiter.Range(3, 10))
+	assert.Assert(t, l3 != nil)
+	assert.DeepEqual(t, slices.Collect(hiter.Range(3, 10)), slices.Collect(hiter.ListAll[int](l3)))
+
+	r1 := ring.New(3)
+	for i := range hiter.Range(0, 3) {
+		r1.Value = i
+		r1 = r1.Next()
+	}
+	r2 := hiter.AppendRingSeq(r1.Prev(), hiter.Range(3, 10))
+	assert.Assert(t, r1 == r2)
+	assert.DeepEqual(t, slices.Collect(seq), slices.Collect(hiter.RingAll[int](r2)))
+	r3 := hiter.CollectRing(hiter.Range(3, 10))
+	assert.Assert(t, r3 != nil)
+	assert.DeepEqual(t, slices.Collect(hiter.Range(3, 10)), slices.Collect(hiter.RingAll[int](r3)))
 }

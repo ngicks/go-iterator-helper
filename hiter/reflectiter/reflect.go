6d0305a97d3a76fe9735d317245d754aebcd6e0d
ngicks <yknt.bsl@gmail.com>
2024-11-30 08:01:23 +0000

    ✨: add reflectiter.Seq/Seq2

--- hiter/reflectiter/reflect.go
+++ hiter/reflectiter/reflect.go
@@ -17,3 +17,13 @@ func AssertValue[V any](seq iter.Seq[reflect.Value]) iter.Seq[V] {
 func AssertValue2[K, V any](seq iter.Seq2[reflect.Value, reflect.Value]) iter.Seq2[K, V] {
 	return adapter.MapIter2(func(k, v reflect.Value) (K, V) { return k.Interface().(K), v.Interface().(V) }, seq)
 }
+
+// Seq wraps [reflect.Value.Seq] with [AssertValue].
+func Seq[V any](rv reflect.Value) iter.Seq[V] {
+	return AssertValue[V](rv.Seq())
+}
+
+// Seq2 wraps [reflect.Value.Seq2] with [AssertValue2].
+func Seq2[K, V any](rv reflect.Value) iter.Seq2[K, V] {
+	return AssertValue2[K, V](rv.Seq2())
+}
--- hiter/reflectiter/reflect_test.go
+++ hiter/reflectiter/reflect_test.go
@@ -18,6 +18,12 @@ func TestAssert(t *testing.T) {
 			rv := reflect.ValueOf(src)
 			return reflectiter.AssertValue[string](hiter.OmitF(rv.Seq2()))
 		},
+		Seqs: []func() iter.Seq[string]{
+			func() iter.Seq[string] {
+				rv := reflect.ValueOf(src)
+				return hiter.OmitF(reflectiter.Seq2[int, string](rv))
+			},
+		},
 		Expected: []string{"foo", "bar", "baz"},
 		BreakAt:  2,
 	}.Test(t)
@@ -26,6 +32,12 @@ func TestAssert(t *testing.T) {
 		Seq: func() iter.Seq2[int, string] {
 			rv := reflect.ValueOf(src)
 			return reflectiter.AssertValue2[int, string](rv.Seq2())
+		},
+		Seqs: []func() iter.Seq2[int, string]{
+			func() iter.Seq2[int, string] {
+				rv := reflect.ValueOf(src)
+				return reflectiter.Seq2[int, string](rv)
+			},
 		},
 		Expected: []hiter.KeyValue[int, string]{{K: 0, V: "foo"}, {K: 1, V: "bar"}, {K: 2, V: "baz"}},
 		BreakAt:  2,

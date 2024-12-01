9d39ddb320238f48a6ad3e5d988ee547110ae88e
ngicks <yknt.bsl@gmail.com>
2024-12-01 13:27:49 +0000

    ✅: extend reader tests
    Thought its logic was somewhat wrong,
    Added tests,
    Turns out it wasn't wrong.

--- hiter/iterreader/reader_test.go
+++ hiter/iterreader/reader_test.go
@@ -57,6 +57,77 @@ func TestReader(t *testing.T) {
 	assert.NilError(t, err)
 }
 
+func TestReader_border(t *testing.T) {
+	factory := func() io.Reader {
+		return iterreader.Reader(
+			func(b string) ([]byte, error) { return []byte(b), nil },
+			iterable.NewResumable(slices.Values([]string{"foo", "bar", "baz"})).IntoIter(),
+		)
+	}
+
+	type readOp struct {
+		readSize        int
+		expectedContent string
+		err             error
+	}
+
+	type testCase struct {
+		name string
+		ops  []readOp
+	}
+
+	for _, tc := range []testCase{
+		{
+			name: "one less",
+			ops: []readOp{
+				{8, "foobarba", nil},
+				{8, "z", io.EOF},
+				{8, "", io.EOF},
+			},
+		},
+		{
+			name: "one more",
+			ops: []readOp{
+				{10, "foobarbaz", io.EOF},
+				{8, "", io.EOF},
+			},
+		},
+		{
+			name: "exact",
+			ops: []readOp{
+				{9, "foobarbaz", nil},
+				{1, "", io.EOF},
+			},
+		},
+		{
+			name: "one by one",
+			ops: []readOp{
+				{1, "f", nil},
+				{1, "o", nil},
+				{1, "o", nil},
+				{1, "b", nil},
+				{1, "a", nil},
+				{1, "r", nil},
+				{1, "b", nil},
+				{1, "a", nil},
+				{1, "z", nil},
+				{1, "", io.EOF},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			r := factory()
+			for _, op := range tc.ops {
+				buf := make([]byte, op.readSize)
+				n, err := r.Read(buf)
+				assert.ErrorIs(t, err, op.err)
+				assert.Equal(t, len(op.expectedContent), n)
+				assert.Equal(t, op.expectedContent, string(buf[:n]))
+			}
+		})
+	}
+}
+
 func TestMarshalerReader(t *testing.T) {
 	times := []time.Time{
 		time.Date(2024, 10, 20, 11, 20, 47, 0, time.UTC),

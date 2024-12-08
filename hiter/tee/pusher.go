b4a3df938c51fd5348b166f101c358df6fcf58cc
ngicks <yknt.bsl@gmail.com>
2024-12-08 08:35:53 +0000

    ✨: add MultiPusher

--- hiter/tee/pusher.go
+++ hiter/tee/pusher.go
@@ -1,6 +1,36 @@
 package tee
 
-import "iter"
+import (
+	"iter"
+)
+
+// MultiPusher is [iter.Seq] equivalent of [io.MultiWriter].
+//
+// Experimental: not tested and might be changed any time.
+func MultiPusher[V any](pushers ...func(V) bool) func(v V) bool {
+	return func(v V) bool {
+		for _, p := range pushers {
+			if !p(v) {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// MultiPusher2 is [iter.Seq2] equivalent of [io.MultiWriter].
+//
+// Experimental: not tested and might be changed any time.
+func MultiPusher2[K, V any](pushers ...func(K, V) bool) func(k K, v V) bool {
+	return func(k K, v V) bool {
+		for _, p := range pushers {
+			if !p(k, v) {
+				return false
+			}
+		}
+		return true
+	}
+}
 
 // Experimental: not tested and might be changed any time.
 func Push[V any](seq iter.Seq[V], f ...func(V) bool) bool {

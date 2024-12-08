9c3be335b2f783cdc21aec788da8b5d7fb3f538c
ngicks <yknt.bsl@gmail.com>
2024-12-08 08:36:08 +0000

    ✅: add tee example

---
+++ example_teeing_test.go
@@ -0,0 +1,41 @@
+package goiteratorhelper_test
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/ngicks/go-iterator-helper/hiter"
+	"github.com/ngicks/go-iterator-helper/hiter/tee"
+	"github.com/ngicks/go-iterator-helper/x/exp/xiter"
+)
+
+func Example_teeing() {
+	src := hiter.Range(0, 5)
+	seqPiped, seq := tee.TeeSeqPipe(0, src)
+
+	var found bool
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		found = hiter.Contains(3, seqPiped.IntoIter())
+		// Don't forget to discard all elements from seq!
+		// Without this, tee could not proceed.
+		hiter.Discard(seqPiped.IntoIter())
+	}()
+
+	for i := range xiter.Map(func(i int) int { return i * i }, seq.IntoIter()) {
+		fmt.Printf("i = %02d\n", i)
+	}
+	wg.Wait()
+	fmt.Printf("\nfound=%t\n", found)
+	// Output:
+	// i = 00
+	// i = 01
+	// i = 04
+	// i = 09
+	// i = 16
+	//
+	// found=true
+}

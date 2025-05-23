19d35fbd7cd0f2a1285cbaeae4027b2f6c512a80
ngicks <yknt.bsl@gmail.com>
2025-05-23 09:53:25 +0900

    ✨: add errbox.Box2, Map, Map2

--- hiter/errbox/box.go
+++ hiter/errbox/box.go
@@ -3,6 +3,14 @@ package errbox
 
 import (
 	"iter"
+
+	"github.com/ngicks/go-iterator-helper/hiter"
+	"github.com/ngicks/go-iterator-helper/x/exp/xiter"
+)
+
+var (
+	_ hiter.IntoIterable[any]       = (*Box[any])(nil)
+	_ hiter.IntoIterable2[any, any] = (*Box2[any, any])(nil)
 )
 
 // Box boxes an input [iter.Seq2][V, error] to [iter.Seq][V] by stripping nil errors from values over the input iterator.
@@ -37,6 +45,13 @@ func New[V any](seq iter.Seq2[V, error]) *Box[V] {
 	}
 }
 
+// Map maps values over the input iterator and put them in Box.
+//
+// See doc comment for [Box] for detailed caveats.
+func Map[V1, V2 any](mapper func(v V1) (V2, error), seq iter.Seq[V1]) *Box[V2] {
+	return New(hiter.Divide(mapper, seq))
+}
+
 // IntoIter returns an iterator which yields values from the input iterator.
 //
 // As the name IntoIter suggests, the iterator is (partially) stateful;
@@ -63,5 +78,58 @@ func (b *Box[V]) IntoIter() iter.Seq[V] {
 // Err returns an error the input iterator has returned.
 // If the iterator has not yet encountered an error, Err returns nil.
 func (b *Box[V]) Err() error {
+	return b.err
+}
+
+// Box2 is like [Box] but with [iter.Seq2].
+//
+// See doc comment for [Box] for detail.
+type Box2[K, V any] struct {
+	err error
+	seq iter.Seq2[hiter.KeyValue[K, V], error]
+}
+
+// New2 is like [New] but returns Box2.
+func New2[K, V any](seq iter.Seq2[hiter.KeyValue[K, V], error]) *Box2[K, V] {
+	return &Box2[K, V]{
+		seq: seq,
+	}
+}
+
+// Map2 maps pairs of values over the input iterator using mapper and put them in Box2.
+//
+// See doc comment for [Box] for detailed caveats.
+func Map2[K1, V1, K2, V2 any](mapper func(K1, V1) (K2, V2, error), seq iter.Seq2[K1, V1]) *Box2[K2, V2] {
+	return New2(xiter.Map2(
+		func(k K1, v V1) (hiter.KeyValue[K2, V2], error) {
+			k2, v2, err := mapper(k, v)
+			return hiter.KeyValue[K2, V2]{K: k2, V: v2}, err
+		},
+		seq,
+	),
+	)
+}
+
+// IntoIter2 is like [Box.IntoIter] but is for Box2.
+func (b *Box2[K, V]) IntoIter2() iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		if b.err != nil {
+			return
+		}
+		for kv, err := range b.seq {
+			if err != nil {
+				b.err = err
+				return
+			}
+			if !yield(kv.K, kv.V) {
+				return
+			}
+		}
+	}
+}
+
+// Err returns an error the input iterator has returned.
+// If the iterator has not yet encountered an error, Err returns nil.
+func (b *Box2[K, V]) Err() error {
 	return b.err
 }
--- hiter/errbox/box_test.go
+++ hiter/errbox/box_test.go
@@ -13,6 +13,19 @@ import (
 	"gotest.tools/v3/assert"
 )
 
+var (
+	errSample = errors.New("sample")
+	expected  = []int{0, 1, 2, 3, 4, 5}
+	expected2 = []hiter.KeyValue[int, int]{
+		{K: 0, V: 5},
+		{K: 1, V: 4},
+		{K: 2, V: 3},
+		{K: 3, V: 2},
+		{K: 4, V: 1},
+		{K: 5, V: 0},
+	}
+)
+
 func TestBox(t *testing.T) {
 	testcase.One[int]{
 		Seq: func() iter.Seq[int] {
@@ -23,7 +36,7 @@ func TestBox(t *testing.T) {
 				),
 			).IntoIter()
 		},
-		Expected: []int{0, 1, 2, 3, 4, 5},
+		Expected: expected,
 		BreakAt:  3,
 	}.Test(t)
 
@@ -35,11 +48,71 @@ func TestBox(t *testing.T) {
 		),
 	)
 
-	assert.DeepEqual(t, []int{0, 1, 2}, slices.Collect(hiter.Limit(3, box.IntoIter())))
+	assert.DeepEqual(t, expected[:3], slices.Collect(hiter.Limit(3, box.IntoIter())))
 	assert.NilError(t, box.Err())
-	assert.DeepEqual(t, []int{0, 1, 2, 3}, slices.Collect(hiter.Limit(4, box.IntoIter())))
+	assert.DeepEqual(t, expected[:4], slices.Collect(hiter.Limit(4, box.IntoIter())))
 	assert.NilError(t, box.Err())
-	assert.DeepEqual(t, []int{0, 1, 2, 3, 4}, slices.Collect(box.IntoIter()))
+	assert.DeepEqual(t, expected[:5], slices.Collect(box.IntoIter()))
 	assert.ErrorIs(t, box.Err(), errSample)
 	assert.DeepEqual(t, []int(nil), slices.Collect(box.IntoIter()))
+}
+
+func TestBox_Map(t *testing.T) {
+	errSample := errors.New("sample")
+	box := errbox.Map2(
+		func(i, j int) (int, int, error) {
+			if i > j {
+				return j, i, errSample
+			}
+			return i, j, nil
+		},
+		hiter.Pairs(hiter.Range(0, 6), hiter.Range(5, -1)),
+	)
+	assert.DeepEqual(t, expected2[:3], hiter.Collect2(box.IntoIter2()))
+	assert.ErrorIs(t, box.Err(), errSample)
+}
+
+func TestBox2(t *testing.T) {
+	testcase.Two[int, int]{
+		Seq: func() iter.Seq2[int, int] {
+			return errbox.New2(
+				hiter.Pairs(
+					hiter.ToKeyValue(hiter.Pairs(hiter.Range(0, 6), hiter.Range(5, -1))),
+					hiter.Repeat(error(nil), -1),
+				),
+			).IntoIter2()
+		},
+		Expected: expected2,
+		BreakAt:  3,
+	}.Test(t)
+
+	box := errbox.New2(
+		hiter.Pairs(
+			hiter.ToKeyValue(hiter.Pairs(hiter.Range(0, 6), hiter.Range(5, -1))),
+			xiter.Concat(hiter.Repeat(error(nil), 5), hiter.Once(errSample)),
+		),
+	)
+
+	assert.DeepEqual(t, expected2[:3], hiter.Collect2(hiter.Limit2(3, box.IntoIter2())))
+	assert.NilError(t, box.Err())
+	assert.DeepEqual(t, expected2[:4], hiter.Collect2(hiter.Limit2(4, box.IntoIter2())))
+	assert.NilError(t, box.Err())
+	assert.DeepEqual(t, expected2[:5], hiter.Collect2(box.IntoIter2()))
+	assert.ErrorIs(t, box.Err(), errSample)
+	assert.DeepEqual(t, []hiter.KeyValue[int, int](nil), hiter.Collect2(box.IntoIter2()))
+}
+
+func TestBox2_Map2(t *testing.T) {
+	errSample := errors.New("sample")
+	box := errbox.Map2(
+		func(i, j int) (int, int, error) {
+			if i > j {
+				return j, i, errSample
+			}
+			return i, j, nil
+		},
+		hiter.Pairs(hiter.Range(0, 6), hiter.Range(5, -1)),
+	)
+	assert.DeepEqual(t, expected2[:3], hiter.Collect2(box.IntoIter2()))
+	assert.ErrorIs(t, box.Err(), errSample)
 }

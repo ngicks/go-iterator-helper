eefbb7dc2b374e12eb79ac1050ab07629e9224ee
ngicks <yknt.bsl@gmail.com>
2025-05-31 17:44:10 +0900

    📝: add notes for Pipe/Pipe2
    TODO: add those defined in x/exp/xiter to hiter

--- hiter/tee/pipe.go
+++ hiter/tee/pipe.go
@@ -15,13 +15,15 @@ var (
 	_ hiter.IntoIterable2[any, any] = (*Pipe2[any, any])(nil)
 )
 
-// Pipe is like [io.Pipe], but conveys values over an iterator.
+// Pipe is similar [io.Pipe]
 type Pipe[V any] struct {
 	c      chan V
 	closed chan struct{}
 }
 
-// Experimental: not tested and might be changed any time.
+// NewPipe creates new Pipe instance.
+// Unlike [io.Pipe], you can choose buffer size of internal channel by putting n.
+// If n is less than or equal to zero, it is unbuffered.
 func NewPipe[V any](n int) *Pipe[V] {
 	if n < 0 {
 		// panic?
@@ -34,6 +36,13 @@ func NewPipe[V any](n int) *Pipe[V] {
 	return p
 }
 
+// Close closes p.
+// After calling Close, all Push failes returnning false,
+// the iterator returned from [*Pipe.IntoIter] stops after buffered values.
+//
+// Close itself is not gorotine safe;
+// it is safe to call Close adn Push from multiple goroutines.
+// But simultaneous multiple calls to Close may panic.
 func (p *Pipe[V]) Close() {
 	select {
 	case <-p.closed:
@@ -44,6 +53,12 @@ func (p *Pipe[V]) Close() {
 	close(p.c)
 }
 
+// Push pushes v to p.
+// Values might queue up in buffered channel if the internal channel was buffered,
+// and might be received by the other end of the pipe
+// by consuming an iterator retrived from [*Pipe.IntoIter].
+//
+// Push may block long if values are not read from the other side of the pipe.
 func (p *Pipe[V]) Push(v V) bool {
 	select {
 	case <-p.closed:
@@ -58,6 +73,9 @@ func (p *Pipe[V]) Push(v V) bool {
 	}
 }
 
+// TryPush is like [*Pipe.Push], but does not block on sending.
+// If [*Pipe.Close] is already called, open will be false.
+// If it would block on sending v, pushed will false.
 func (p *Pipe[V]) TryPush(v V) (open, pushed bool) {
 	select {
 	case <-p.closed:
@@ -72,15 +90,22 @@ func (p *Pipe[V]) TryPush(v V) (open, pushed bool) {
 	}
 }
 
+// IntoIter returns the reading side of pipe.
+// The iterator yields values sent by [*Pipe.Push].
+// The iterator stops only after [*Pipe.Close] is called.
 func (p *Pipe[V]) IntoIter() iter.Seq[V] {
 	return hiter.Chan(context.Background(), p.c)
 }
 
+// Pipes is like [Pipe], but it pipes key-value pairs.
 type Pipe2[K, V any] struct {
 	c      chan hiter.KeyValue[K, V]
 	closed chan struct{}
 }
 
+// NewPipe2 creates new Pipe2 instance.
+// Unlike [io.Pipe], you can choose buffer size of internal channel by putting n.
+// If n is less than or equal to zero, it is unbuffered.
 func NewPipe2[K, V any](n int) *Pipe2[K, V] {
 	if n < 0 {
 		// panic?
@@ -93,6 +118,13 @@ func NewPipe2[K, V any](n int) *Pipe2[K, V] {
 	return p
 }
 
+// Close closes p.
+// After calling Close, all Push fails returnning false,
+// the iterator returned from [*Pipe.IntoIter] stops after buffered values.
+//
+// Close itself is not gorotine safe;
+// it is safe to call Close adn Push from multiple goroutines.
+// But simultaneous multiple calls to Close may panic.
 func (p *Pipe2[K, V]) Close() {
 	select {
 	case <-p.closed:
@@ -103,6 +135,12 @@ func (p *Pipe2[K, V]) Close() {
 	close(p.c)
 }
 
+// Push pushes a pair of k and v to p.
+// Pairs might queue up in buffered channel if the internal channel was buffered,
+// and will be received by the other end of the pipe
+// by consuming an iterator retrived from [*Pipe.IntoIter].
+//
+// Push may block long if values are not read from the other side of the pipe.
 func (p *Pipe2[K, V]) Push(k K, v V) bool {
 	select {
 	case <-p.closed:
@@ -117,6 +155,9 @@ func (p *Pipe2[K, V]) Push(k K, v V) bool {
 	}
 }
 
+// TryPush is like [*Pipe2.Push], but does not block on sending.
+// If [*Pipe.Close] is already called, open will be false.
+// If it would block on sending k adn v, pushed will false.
 func (p *Pipe2[K, V]) TryPush(k K, v V) (open, pushed bool) {
 	select {
 	case <-p.closed:
@@ -131,6 +172,9 @@ func (p *Pipe2[K, V]) TryPush(k K, v V) (open, pushed bool) {
 	}
 }
 
+// IntoIter2 returns the reading side of pipe.
+// The iterator yields k-v pairs sent by [*Pipe.Push].
+// The iterator stops only after [*Pipe.Close] is called.
 func (p *Pipe2[K, V]) IntoIter2() iter.Seq2[K, V] {
 	return hiter.FromKeyValue(hiter.Chan(context.Background(), p.c))
 }
